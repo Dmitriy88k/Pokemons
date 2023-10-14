cb3a6f6b1f4318d043676f7e5aa8ce203b2c8a70
Dmitriy Khegay <dmitriy88k@gmail.com>
2023-10-14 19:04:07 -0400

    finished with adding info into the post page.

--- client/js/post-page.js
+++ client/js/post-page.js
@@ -1,8 +1,29 @@
+const container = document.getElementById('container');
+
 //получить id post из url
-
 const splittedHref = location.href.split('/');
-const name = splittedHref[splittedHref.length - 1];
+const post = splittedHref[splittedHref.length - 1];
+console.log("post is", post);
 
-console.log(name);
 //сделать запрос на https://jsonplaceholder.typicode.com/posts/${post.id} с id текущего поста
+const fetchDetails = () => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${post}`).then(res => 
+    res.json()
+  );
+};
+
+
 //отобразить на странице
+fetchDetails().then(details => {
+  const title = document.createElement('h3');
+  title.innerText = `Title: ${details.title}`;
+  title.style.color="white";
+
+  const userId = document.createElement("p");
+  userId.innerText = `Post# ${details.id}`;
+  userId.style.color="red";
+  
+  container.appendChild(title);
+  container.appendChild(userId);
+
+});
